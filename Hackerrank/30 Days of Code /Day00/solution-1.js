de09ee1521cad50e619f2b379bf0f37b1dc7431a
ridho muhammad <ulunmuhammadridho@gmail.com>
2018-12-06 06:50:32 +0700

    master finishing 1 day

---
+++ Hackerrank/30 Days of Code /Day00/solution-1.js
@@ -0,0 +1,5 @@
+function processData(inputString) {
+    // This line of code prints the first line of output
+    console.log("Hello, World.", "\n"+inputString);
+    // Write the second line of output that prints the contents of 'inputString' here.
+}
